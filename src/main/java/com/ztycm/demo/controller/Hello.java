b7d9ed2efe62e5d7242fb88611de8e6071c0af80
ycm120 <18710615070@163.com>
2019-06-02 09:44:49 +0800

    test server

--- src/main/java/com/ztycm/demo/controller/Hello.java
+++ src/main/java/com/ztycm/demo/controller/Hello.java
@@ -3,17 +3,20 @@ package com.ztycm.demo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class Hello {
 
-	@RequestMapping("test")
-	public Map<String, String> test(){
+	@GetMapping(value = "/test")
+	public Map<String, String> test(@RequestParam("id") String id){
 		Map<String, String> result = new HashMap<>();
 		result.put("name", "周彤");
+		result.put("id", id);
 		return result;
 	} 
 }
